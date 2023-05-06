980e18ee6af97e7b78de576fb17fd95a93199913
Amminn <vrb.nnn@gmail.com>
2023-05-06 12:12:56 +0100

    style: Trim spaces, Format Document

--- index.js
+++ index.js
@@ -1,83 +1,102 @@
-const input = document.getElementById('input')
-const submitBtn = document.getElementById('submit')
-const moviePlace = document.querySelector('.movies .container')
-const film = document.querySelector('.film')
-let addBtns = ''
-let searchList = []
-let preLocalStorage = []
+const input = document.getElementById("input");
+const submitBtn = document.getElementById("submit");
+const moviePlace = document.querySelector(".movies .container");
+const film = document.querySelector(".film");
+
+let addBtns = "";
+let searchList = [];
+let preLocalStorage = [];
 // preLocalStorage = JSON.parse(window.localStorage.getItem('movies'));
-JSON.parse(window.localStorage.getItem('movies')) === null ? [] : preLocalStorage = JSON.parse(window.localStorage.getItem('movies'))
+JSON.parse(window.localStorage.getItem("movies")) === null
+  ? []
+  : (preLocalStorage = JSON.parse(window.localStorage.getItem("movies")));
 
-submitBtn.addEventListener('click', (e) => {
-	/* Prevent the form from reloading the page when submit */
-	e.preventDefault()
+submitBtn.addEventListener("click", (e) => {
+  /* Prevent the form from reloading the page when submit */
+  e.preventDefault();
 
-	/* Getting the value of the input, then clear it*/
-	let title = input.value
-	input.value = ''
+  /* Getting the value of the input, then clear it*/
+  let title = input.value;
+  input.value = "";
 
-	/* Using async await to make sure the fetch is done working before invoking other functions */
-	async function searchForMovies() {
-		const response = await fetch(`https://www.omdbapi.com/?apikey=eedb40e&s=${title}`);
-		const data = await response.json();
+  /* Using async await to make sure the fetch is done working before invoking other functions */
+  async function searchForMovies() {
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=eedb40e&s=${title}`
+    );
+    const data = await response.json();
 
-		/* If there is a mistake */
-		if (data.Response === "False") {
-			// input.value = "This movie is not found"
-			moviePlace.innerHTML = `
+    /* If there is a mistake */
+    if (data.Response === "False") {
+      // input.value = "This movie is not found"
+      moviePlace.innerHTML = `
 				<div class="film">
 					<h2>Unable to find what you’re looking for. Please try another search.</h2>
 				</div>
-			`
-		/* If we get the data back */ 
-		} else {
-			moviePlace.innerHTML = ''
-			searchList = data.Search
+			`;
+      /* If we get the data back */
+    } else {
+      moviePlace.innerHTML = "";
+      searchList = data.Search;
 
-			/* Looping on the movies title to get all the info */
-			for (const item of searchList) {
+      /* Looping on the movies title to get all the info */
+      for (const item of searchList) {
+        const resp = await fetch(
+          `https://www.omdbapi.com/?i=${item.imdbID}&apikey=eedb40e`
+        );
+        const secondData = await resp.json();
 
-				const resp = await fetch(`https://www.omdbapi.com/?i=${item.imdbID}&apikey=eedb40e`)
-				const secondData = await resp.json();  
+        /* Checking whether the Plot has more than 133 letter, if it is show only 133 letter then add '...' in the end */
+        const content =
+          secondData.Plot.length >= 133
+            ? secondData.Plot.substring(0, 133) + "..."
+            : secondData.Plot;
 
-				/* Checking whether the Plot has more than 133 letter, if it is show only 133 letter then add '...' in the end */
-				const content = secondData.Plot.length >= 133 ? secondData.Plot.substring(0, 133) + "..." : secondData.Plot
-
-				/* Rendering the movies */
-				moviePlace.innerHTML += (`
+        /* Rendering the movies */
+        moviePlace.innerHTML += `
 						<article class="movie" id="${secondData.imdbID}">
 						<!--  <p class="imdbID" style="display: none;">${secondData.imdbID}</p> -->
 							<div class="image">
 								<img src="${secondData.Poster}" alt="${secondData.Title}'s Poster">
 							</div>
 							<div class="text">
-								<h2>${secondData.Title} <span class="rate"><img src="./images/star.png" alt=""> ${secondData.imdbRating}</span></h2>
+								<h2>${
+                  secondData.Title
+                } <span class="rate"><img src="./images/star.png" alt=""> ${
+          secondData.imdbRating
+        }</span></h2>
 								<p class="duration-type">
 									<span class="duration">${secondData.Runtime}</span>
 									<span class="type">${secondData.Genre}</span>
-									<span class="${preLocalStorage.includes(secondData.imdbID) ? '' : 'add'}" id="${secondData.imdbID}">${preLocalStorage.includes(secondData.imdbID) ? "<span><img src='./images/done.png' alt=''>Added</span>" : "<img src='./images/add.png' alt=''>Watchlist"}</span>
+									<span class="${preLocalStorage.includes(secondData.imdbID) ? "" : "add"}" id="${
+          secondData.imdbID
+        }">${
+          preLocalStorage.includes(secondData.imdbID)
+            ? "<span><img src='./images/done.png' alt=''>Added</span>"
+            : "<img src='./images/add.png' alt=''>Watchlist"
+        }</span>
 								</p>
 								<p class="content">${content}</p>
 							</div>
 						</article>
-				`)
-			}
-		}
+				`;
+      }
+    }
 
-		addBtns = document.querySelectorAll('.add')
-		addBtns.forEach(element => {
-			element.addEventListener('click', function adding() {
-				element.removeEventListener('click', adding)
-				let imbdID = element.id
-				element.innerHTML = "<span><img src='./images/done.png' alt=''>Added</span>"
-				preLocalStorage.includes(imbdID) ? '' : preLocalStorage.unshift(imbdID)
-				
-				localStorage.removeItem('movies')
-				window.localStorage.setItem('movies', JSON.stringify(preLocalStorage))
-			})
-		})
-	} /* both fetches ends here */
+    addBtns = document.querySelectorAll(".add");
+    addBtns.forEach((element) => {
+      element.addEventListener("click", function adding() {
+        element.removeEventListener("click", adding);
+        let imbdID = element.id;
+        element.innerHTML =
+          "<span><img src='./images/done.png' alt=''>Added</span>";
+        preLocalStorage.includes(imbdID) ? "" : preLocalStorage.unshift(imbdID);
 
-	searchForMovies();
+        localStorage.removeItem("movies");
+        window.localStorage.setItem("movies", JSON.stringify(preLocalStorage));
+      });
+    });
+  } /* both fetches ends here */
 
-})
+  searchForMovies();
+});
--- watchlist.js
+++ watchlist.js
@@ -1,77 +1,83 @@
-const moviePlace = document.querySelector('.movies .container')
+const moviePlace = document.querySelector(".movies .container");
 
 /* getting the movie list data from localstorage */
-let movieList = JSON.parse(localStorage.getItem('movies'));
+let movieList = JSON.parse(localStorage.getItem("movies"));
 
 /* Rendering the movies */
 async function render() {
-    /* looping on all the movies of the list using their only imdbID */
-    for (const item of movieList) {
-        const resp = await fetch(`https://www.omdbapi.com/?i=${item}&apikey=eedb40e`)
-        const movieDate = await resp.json();  
+  /* looping on all the movies of the list using their only imdbID */
+  for (const item of movieList) {
+    const resp = await fetch(
+      `https://www.omdbapi.com/?i=${item}&apikey=eedb40e`
+    );
+    const movieDate = await resp.json();
 
-        /* Checking if the Plot has more than 133 letter, if it is show only 133 and add '...' in the end */
-        const content = movieDate.Plot.length >= 133 ? movieDate.Plot.substring(0, 133) + "..." : movieDate.Plot
+    /* Checking if the Plot has more than 133 letter, if it is show only 133 and add '...' in the end */
+    const content =
+      movieDate.Plot.length >= 133
+        ? movieDate.Plot.substring(0, 133) + "..."
+        : movieDate.Plot;
 
-        /* Rendering the movies with html css*/
-        moviePlace.innerHTML += (`
+    /* Rendering the movies with html css*/
+    moviePlace.innerHTML += `
             <article class="movie" id="${movieDate.imdbID}">
             <!--  <p class="imdbID" style="display: none;">${movieDate.imdbID}</p> -->
                 <div class="image">
-                    <img src="${movieDate.Poster}" alt="${movieDate.Title}'s Poster">
+                  <img src="${movieDate.Poster}" alt="${movieDate.Title}'s Poster">
                 </div>
                 <div class="text">
                     <h2>${movieDate.Title} <span class="rate"><img src="./images/star.png" alt=""> ${movieDate.imdbRating}</span></h2>
                     <p class="duration-type">
-                        <span class="duration">${movieDate.Runtime}</span>
-                        <span class="type">${movieDate.Genre}</span>
-                        <span class="add" id="${movieDate.imdbID}"><img src='./images/remove.png' alt=''>remove</span>
+                      <span class="duration">${movieDate.Runtime}</span>
+                      <span class="type">${movieDate.Genre}</span>
+                      <span class="add" id="${movieDate.imdbID}"><img src='./images/remove.png' alt=''>remove</span>
                     </p>
                     <p class="content">${content}</p>
                 </div>
             </article>
-        `)
-    }
-    removeBtns = document.querySelectorAll('.add')
-    removeBtns.forEach(element => {
-      element.addEventListener('click', function adding() {
-        element.removeEventListener('click', adding)
-        let imbdID = element.id
-        // console.log(imbdID)
-        document.getElementById(`${imbdID}`).remove()
-        element.innerHTML = `<span class="add"><img src='./images/remove.png' alt=''>removed</span>`
-        // preLocalStorage.includes(imbdID) ? console.log('this element is already there') : preLocalStorage.unshift(imbdID)
+        `;
+  }
+  removeBtns = document.querySelectorAll(".add");
+  removeBtns.forEach((element) => {
+    element.addEventListener("click", function adding() {
+      element.removeEventListener("click", adding);
+      let imbdID = element.id;
+      // console.log(imbdID)
+      document.getElementById(`${imbdID}`).remove();
+      element.innerHTML = `<span class="add"><img src='./images/remove.png' alt=''>removed</span>`;
+      // preLocalStorage.includes(imbdID) ? console.log('this element is already there') : preLocalStorage.unshift(imbdID)
 
-        /* Removing the movie from the list */
-        let movieDelete = movieList.indexOf(imbdID);
-        movieList.splice(movieDelete, 1); 
+      /* Removing the movie from the list */
+      let movieDelete = movieList.indexOf(imbdID);
+      movieList.splice(movieDelete, 1);
 
-        // Deleting localStorage data and then submitting the new data 
-        localStorage.removeItem('movies');
-        window.localStorage.setItem('movies', JSON.stringify(movieList));
+      // Deleting localStorage data and then submitting the new data
+      localStorage.removeItem("movies");
+      window.localStorage.setItem("movies", JSON.stringify(movieList));
 
-        // In case there is no movie offer the user to go back to the search page  
-        movieList === null || movieList.length == 0 ? moviePlace.innerHTML = `
+      // In case there is no movie offer the user to go back to the search page
+      movieList === null || movieList.length == 0
+        ? (moviePlace.innerHTML = `
           <div class="film">
             <h2>Your watchlist is looking a little empty...</h2>
             <p class="add">
               <img src="./images/add.png" alt="" />
               <a href="./index.html">Let’s add some movies!</a>
             </p>
-          </div>` : ''
-      })
-    })
+          </div>`)
+        : "";
+    });
+  });
 }
 
 /* Checking if the localStorage has data if true call the render function of not offer the user to go back to the search page */
-movieList === null || movieList.length == 0 ? moviePlace.innerHTML = `
+movieList === null || movieList.length == 0
+  ? (moviePlace.innerHTML = `
   <div class="film">
     <h2>Your watchlist is looking a little empty...</h2>
     <p class="add">
       <img src="./images/add.png" alt="" />
       <a href="./index.html">Let’s add some movies!</a>
     </p>
-  </div>`
-  : render()
-
-
+  </div>`)
+  : render();
